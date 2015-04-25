f29dfb30d964e56f9da80a85cd30fb88dcccab24
Daytron <ryangilera@gmail.com>
2015-04-25 15:47:04 +0100

    Bring two UI screens together in the parent component

--- src/main/java/com/github/daytron/twaattin/TwaattinUI.java
+++ src/main/java/com/github/daytron/twaattin/TwaattinUI.java
@@ -1,3 +1,26 @@
+/* 
+ * The MIT License
+ *
+ * Copyright 2015 Ryan Gilera.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.github.daytron.twaattin;
 
 import javax.servlet.annotation.WebServlet;
@@ -7,38 +30,35 @@ import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.annotations.Widgetset;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Label;
+import com.vaadin.ui.HorizontalSplitPanel;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
 
 /**
  *
  */
 @Theme("mytheme")
 @Widgetset("com.github.daytron.twaattin.MyAppWidgetset")
-public class MyUI extends UI {
+public class TwaattinUI extends UI {
+    private static final long serialVersionUID = 1L;
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        final VerticalLayout layout = new VerticalLayout();
-        layout.setMargin(true);
-        setContent(layout);
-
-        Button button = new Button("Click Me");
-        button.addClickListener(new Button.ClickListener() {
-            @Override
-            public void buttonClick(ClickEvent event) {
-                layout.addComponent(new Label("Thank you for clicking"));
-            }
-        });
-        layout.addComponent(button);
+        HorizontalSplitPanel panel = new HorizontalSplitPanel();
+        
+        panel.setFirstComponent(new LoginScreen());
+        
+        TimelineScreen aTimelineScreen = new TimelineScreen();
+        panel.setSecondComponent(aTimelineScreen);
+        aTimelineScreen.fillTweets();
+        
+        panel.setSplitPosition(300,Unit.PIXELS);
+        
+        setContent(panel);
 
     }
 
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
-    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
+    @VaadinServletConfiguration(ui = TwaattinUI.class, productionMode = false)
     public static class MyUIServlet extends VaadinServlet {
     }
 }
